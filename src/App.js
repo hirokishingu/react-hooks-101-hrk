b1d05bf308edc0f194d8fbf582595e2cab9918af
hirokishingu <hrk.bass.526@gmail.com>
2019-07-07 18:00:37 +0900

    implements useEFfect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = props => {
   const [state, setState] = useState(props)
   const { name, price } = state
+
+  useEffect(() => {
+    console.log("useEffect is invoked.")
+  })
+
+  useEffect(() => {
+    console.log("This is like componentDidMount")
+  }, [])
+
+  useEffect(() => {
+    console.log("This calls back names only.")
+  }, [name])
 
   return (
     <>
