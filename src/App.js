c342a8d3d75929fa5d5d821979256226aeb2cc4d
hirokishingu <hrk.bass.526@gmail.com>
2019-07-07 17:16:36 +0900

    implemnet component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,42 +1,27 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0)
-  console.log({count})
-  console.log({setCount})
+const App = props => {
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const increment = () => setCount(count + 1)
-
-  const decrement = () => setCount(count- 1)
-
-  const increment2 = () => setCount(previousCount => previousCount + 1)
-  const decrement2 = () => setCount(previousCount => previousCount - 1)
-  const reset = () => setCount(0)
-  const by = () => setCount(count * 2)
-  const san = () => setCount(count => {
-      return count % 3 == 0 ? count / 3 : count
-  })
-
-  return (
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
+    return (
     <>
-      <div>
-        count: {count}
-      </div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={by}>x2</button>
-        <button onClick={san}>/3if3</button>
-      </div>
+      <p>now {name} is {price}.</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>reset</button>
+      <input value={name} onChange={e => setName(e.target.value)} />
     </>
   );
+}
+
+App.defaultProps = {
+  name: "sample",
+  price: 1000
 }
 
 export default App;
